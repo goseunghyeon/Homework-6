a32e306f554997d5601e62ece161c670cac57bf2
goseunghyeon <74235149+goseunghyeon@users.noreply.github.com>
2021-04-11 16:24:28 +0900

    Update singly-linked-list.c

--- singly-linked-list.c
+++ singly-linked-list.c
@@ -139,12 +139,13 @@ int freeList(headNode* h) {
 
 /**
  * list 처음에 key에 대한 노드하나를 추가
+ * 처음에 넣기위해서 노드를 만들고 h를 이용해서 처음 노드의 주소를 만든 노드의link값으로 연결
+ * h->first에는 새로만든 노드의 주소값을 저장해준다.
  */
 int insertFirst(headNode* h, int key) {
-
+	
 	listNode* node = (listNode*)malloc(sizeof(listNode));
 	node->key = key;
-
 	node->link = h->first;
 	h->first = node;
 
@@ -153,13 +154,22 @@ int insertFirst(headNode* h, int key) {
 
 
 /* 리스트를 검색하여, 입력받은 key보다 큰값이 나오는 노드 바로 앞에 삽입 */
-/*첫번째 처리후 중간처리 그이후에도 없다면 마지막에 노드삽입*/
+/*이런 경우는 포인터 변수가 3개가 필요하다
+select:생성한 노드와 키값을 비교할 노드를 지정할 포인터변수
+prevoew:노드의 삽입을 위해 select바로 이전 노드의 주소를 저장하고 있는 포인터변수
+node:지정된 key값으로 저장할 노드의 주소를 저장할 포인터변수
+select를 이용하여 노드와 만든노드의 key값을 비교하면서 적절한 위치에 노드를 삽입하고
+preview의 link에는 새로운 노드의 주소를 할당하고 새로운 노드의 link에는 select노드의 주소를 할당한다*/
 int insertNode(headNode* h, int key) {
 	listNode* select = h->first;
 	listNode* preview = NULL;
 	listNode* node = (listNode*)malloc(sizeof(listNode));
 	node->key = key;
 	node->link = NULL;
+	if (h->first == NULL) {
+		h->first = node;
+		return 0;
+	}
 	if (node->key <= select->key) {
 		node->link = select;
 		h->first = node;
@@ -182,6 +192,9 @@ int insertNode(headNode* h, int key) {
 
 /**
  * list에 key에 대한 노드하나를 추가
+ * 노드를 key값을 저장해서 하나를 동적할당을 이용해서 만든다
+ *마지막에 노드를 추가하므로 포인터 변수를 이용해서 가장 마지막의
+ * 노드를 찾고 해당 노드의 link와 생성한 노드를 연결해준다
  */
 int insertLast(headNode* h, int key) {
 	listNode* select;
@@ -204,37 +217,116 @@ int insertLast(headNode* h, int key) {
 
 /**
  * list의 첫번째 노드 삭제
+ * 첫번쨰 노드가 없는 경우 바로 리턴한다
+ * 그게 아니고 노드가 한개만 있는경우 해당 노드를
+ * free하고 리턴한다
+ * 그것도 아니면 노드가2개이상 존재하므로 가장 마지막 노드로 
+ * 포인터를 이동시키고 해당 노드를 free해준다
  */
 int deleteFirst(headNode* h) {
-
-
+	listNode* select;
+	select = h->first;
+	if (h->first == NULL) {
+		printf("no data\n");
+		return 0;
+	}
+	if (select->link == NULL) {
+		free(select);
+		h->first = NULL;
+	}
+	else {
+		h->first = select->link;
+		free(select);
+	}
 	return 0;
 }
 
 
 /**
  * list에서 key에 대한 노드 삭제
+ * 노드가 없는 경우는 바로 리턴해준다
+ * 제거의 경우는 select를 이용해서 입력받은 key값과 동일한 노드를 찾고
+ * 해당 노드와 해당 이전노드의 주소를 저장하는 preview를 이용하여 select다음
+ * 노드와 preview의 link를 연결해주고 select를 free해서 해당 노드를 메모리에서
+ * 해제해준다
  */
 int deleteNode(headNode* h, int key) {
-
+	listNode* select = h->first;
+	listNode* preview = NULL;
+	if (h->first == NULL)
+		return 0;
+	if (key == select->key) {
+		deleteFirst(h);
+		return 0;
+	}
+	else {
+		for (; select; select = select->link) {
+			if (key == select->key) {
+				preview->link = select->link;
+				free(select);
+				break;
+			}
+			preview = select;
+		}
+		if (select == NULL) {
+			printf("no such data\n");
+		}
+	}
 	return 0;
 
 }
 
 /**
  * list의 마지막 노드 삭제
+ * 일단 노드가 아무것도 없는 경우 처리해주고
+ * 마지막 노드의 조건 마지막 노드의 link는 null임을 
+ * 사용해서 select포인터 변수를 마지막 노드까지 이동시키고 
+ 마지막 노드의 데이터를 free시킨다 그리고 이전노드의 link의 
+ 주소를 null즉 이전노드를 마지막 노드로 설정해준다.
  */
 int deleteLast(headNode* h) {
-
+	if (h->first == NULL)
+		return 0;
+	listNode* select = h->first;
+	listNode* preview = NULL;
+	for (; select; select = select->link) {
+		if (select->link == NULL) {
+			free(select);
+			preview->link = NULL;
+			break;
+		}
+		preview = select;
+	}
 	return 0;
 }
 
 
 /**
  * 리스트의 링크를 역순으로 재 배치
+ * 리스트를 역순으로 배치하기 위해서 포인터변수 3개를 추가로 선언
+ * middle: 노드의 연결을 바꿀포인터
+ * next:middle의 노드를 바꾸는 경우 middle다음 노드를 지칭하기위함
+ * preview:middle의 link와 역으로 연결할 노드를 지칭하는 포인터
+ * 반복으로 수행후 마지막 노드의 연결을 수정하면 next는 null이고 
+ * middle와 preview는 같은 노드를 지칭하게 되고 해당 노드와 h->first를
+ * 연결을 해줘야 역순으로 연결이 완료된다.
  */
 int invertList(headNode* h) {
+	listNode* next = h->first;;
+	listNode* middle=NULL;
+	listNode* preview=NULL;
+	if (h->first == NULL)
+		return 0;
+	while (next != NULL) {
+		middle = next;
+		next = next->link;
+		middle->link = preview;
+		preview = middle;
+		if (next == NULL) {
+			h->first = preview;
+		}
 
+	}
 	return 0;
 }
 
